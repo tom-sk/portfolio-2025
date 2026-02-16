651c95cf1ffc1c17adfbd352b44362a4451da18d
tom <thomasscullyking@hotmail.com>
2026-02-16 10:19:58 +0000

    Updated search

--- config/statamic/search.php
+++ config/statamic/search.php
@@ -27,7 +27,7 @@ return [
 
         'default' => [
             'driver' => 'local',
-            'searchables' => 'all',
+            'searchables' => ['collections:*'],
             'fields' => ['title'],
         ],
 
